571ce6014fc2475fe6ee509a12ebb44440435290
Walle93 <Dynasty_iii@hotmail.com>
2017-04-24 13:26:06 +0200

    all_offline_devices
    En metod som skriver ut alla offline devices.

--- app/app.component.js
+++ app/app.component.js
@@ -15,21 +15,48 @@ exports.Device = Device;
 var DEVICES = {
     "devices": [{
             "id": 1,
-            "name": "king1",
+            "name": "Device 1",
             "location": "rum21",
-            "status": false
+            "status": false,
+            "signalStrength": -90,
+            "battery": 65,
+            "lastSeen": "15 min"
         },
         {
             "id": 2,
-            "name": "king2",
+            "name": "Device 2",
             "location": "rum22",
-            "status": false
+            "status": false,
+            "signalStrength": -62,
+            "battery": 0,
+            "lastSeen": "10 min"
         },
         {
             "id": 3,
-            "name": "king3",
+            "name": "Device 3",
             "location": "rum23",
-            "status": true
+            "status": true,
+            "signalStrength": -53,
+            "battery": 5,
+            "lastSeen": "1 min"
+        },
+        {
+            "id": 4,
+            "name": "Device 4",
+            "location": "rum23",
+            "status": false,
+            "signalStrength": -54,
+            "battery": 0,
+            "lastSeen": "10 min"
+        },
+        {
+            "id": 5,
+            "name": "Device 5",
+            "location": "rum23",
+            "status": false,
+            "signalStrength": -80,
+            "battery": 83,
+            "lastSeen": "1 min"
         }
     ]
 };
@@ -42,9 +69,66 @@ var AppComponent = (function () {
         this.selectedDevice = device;
     };
     AppComponent.prototype.check_offline = function (device) {
-        console.log(device);
-        var obj = device;
-        if (obj.status == true) {
+        if (device.status == false) {
+            return true;
+        }
+    };
+    AppComponent.prototype.all_offline_devices = function (device) {
+        if (device.status == false) {
+            return device.name;
+        }
+    };
+    AppComponent.prototype.devices_online = function (devices) {
+        var count = 0;
+        for (var _i = 0, _a = devices.devices; _i < _a.length; _i++) {
+            var device = _a[_i];
+            if (device.status == true) {
+                count = count + 1;
+            }
+        }
+        var online_percent = (count / devices.devices.length) * 100;
+        return Math.round(online_percent);
+    };
+    AppComponent.prototype.devices_offline = function (devices) {
+        var count = 0;
+        for (var _i = 0, _a = devices.devices; _i < _a.length; _i++) {
+            var device = _a[_i];
+            if (device.status == false) {
+                count = count + 1;
+            }
+        }
+        var online_percent = (count / devices.devices.length) * 100;
+        return Math.round(online_percent);
+    };
+    AppComponent.prototype.warning_devices = function (devices) {
+        var count = 0;
+        for (var _i = 0, _a = devices.devices; _i < _a.length; _i++) {
+            var device = _a[_i];
+            if ((device.battery < 20) && (device.battery > 0)) {
+                count = count + 1;
+            }
+        }
+        var online_percent = (count / devices.devices.length) * 100;
+        return Math.round(online_percent);
+    };
+    AppComponent.prototype.check_location = function (device) {
+        //skapa ny lista för varje location och sätt location som header ovanför respektive lista
+        if (device["location"] == "rum22") {
+            return true;
+        }
+    };
+    AppComponent.prototype.no_duplicates = function (device) {
+        var mySelect = document.getElementById("device_location_list");
+        var mySelectLength = mySelect.length - 1;
+        console.log(mySelect);
+        if (mySelect.length != 0) {
+            for (var i = 0; i < mySelect.length; i++) {
+                if (mySelect.options[mySelectLength].value != device.location) {
+                    return true;
+                }
+            }
+        }
+        else {
             return true;
         }
     };
@@ -53,8 +137,8 @@ var AppComponent = (function () {
 AppComponent = __decorate([
     core_1.Component({
         selector: 'my-app',
-        template: "\n  <h1>Hello {{name}}</h1>\n  <ul>\n    <ng-container *ngFor=\"let device of devices.devices\">\n      <li class=\"list-item\" *ngIf=\"check_offline(device)\" (click)=\"onSelect(device)\">\n        <span>{{device.id}}: {{device.name}}</span>\n      </li>\n    </ng-container>\n  </ul>\n  \n  <div *ngIf=\"selectedDevice\">\n    <p>{{ selectedDevice.name }}</p>\n  </div>",
-        styles: ["\n    .list-item {\n      list-style: none;\n      padding: 10px;\n      border: 1px solid rgba(0,0,0,0.1);\n      border-radius: 25px;\n      margin-bottom: 10px;\n      width: 25%;\n    }"]
+        template: "\n  <h1>Hello {{name}}</h1>\n  <div class=\"location-box\">\n    <select id=\"device_location_list\">\n      <ng-container *ngFor=\"let device of devices.devices\">\n        <option>{{device.location}}</option>\n      </ng-container>\n    </select>\n\n    <ng-container *ngFor=\"let device of devices.devices\">\n        <div class=\"list-item\" *ngIf=\"check_offline(device)\" (click)=\"onSelect(device)\">\n          <span>{{all_offline_devices(device)}}</span>\n          <span class=\"spawn\">{{ device.status }}</span>\n        </div>\n    </ng-container>\n  </div>\n\n  <div *ngIf=\"selectedDevice\">\n    <ul>\n      <li>{{ selectedDevice.signalStrength }}</li>\n      <li>{{ selectedDevice.battery }}</li>\n      <li>{{ selectedDevice.lastSeen }}</li>\n    </ul>\n  </div>\n  <h2>Systemstatus</h2>\n    <p>{{ devices_online(devices) }} % of the devices are online</p>\n    <p>{{ devices_offline(devices) }} % of the devices are offline</p>\n    <p>{{ warning_devices(devices) }} % of the devices have warnings</p>",
+        styles: ["\n    .list-item {\n      padding: 10px;\n      border: 1px solid rgba(0,0,0,0.1);\n      border-radius: 25px;\n      margin-bottom: 10px;\n      width: 25%;\n      display: flex;\n    }\n    \n    .spawn {\n      margin-left: auto;\n    }\n    \n    .location-box {\n      padding: 10px;\n      border: solid 1px rgba(0,0,0,0.2);\n    }\n    "]
     })
 ], AppComponent);
 exports.AppComponent = AppComponent;
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 export class Device {
-  id: number;
-  name: string;
-  location: string;
-  status: boolean;
+    id: number;
+    name: string;
+    location: string;
+    status: boolean;
 }
 
 
@@ -13,54 +13,103 @@ let DEVICES = {
   
 	"devices": [{
 			"id": 1,
-			"name": "king1",
+			"name": "Device 1",
 			"location": "rum21",
-			"status": false
-
+            "status": false,
+            "signalStrength": -90,
+            "battery": 65,
+            "lastSeen": "15 min"
 		},
 		{
 			"id": 2,
-			"name": "king2",
+			"name": "Device 2",
 			"location": "rum22",
-			"status": false
+            "status": false,
+            "signalStrength": -62,
+            "battery": 0,
+            "lastSeen": "10 min"
 		},
 		{
 			"id": 3,
-			"name": "king3",
+			"name": "Device 3",
 			"location": "rum23",
-			"status": true
-		}
-	]
-
+            "status": true,
+            "signalStrength": -53,
+            "battery": 5,
+            "lastSeen": "1 min"
+        },
+        {
+            "id": 4,
+            "name": "Device 4",
+            "location": "rum23",
+            "status": false,
+            "signalStrength": -54,
+            "battery": 0,
+            "lastSeen": "10 min"
+        },
+        {
+            "id": 5,
+            "name": "Device 5",
+            "location": "rum23",
+            "status": false,
+            "signalStrength": -80,
+            "battery": 83,
+            "lastSeen": "1 min"
+        }
+    ]
 
 }
 
 
-
 @Component({
-  selector: 'my-app',
-  template: `
+    selector: 'my-app',
+    template: `
   <h1>Hello {{name}}</h1>
-  <ul>
+  <div class="location-box">
+    <select id="device_location_list">
+      <ng-container *ngFor="let device of devices.devices">
+        <option>{{device.location}}</option>
+      </ng-container>
+    </select>
+
     <ng-container *ngFor="let device of devices.devices">
-      <li class="list-item" *ngIf="check_offline(device)" (click)="onSelect(device)">
-        <span>{{device.id}}: {{device.name}}</span>
-      </li>
+        <div class="list-item" *ngIf="check_offline(device)" (click)="onSelect(device)">
+          <span>{{all_offline_devices(device)}}</span>
+          <span class="spawn">{{ device.status }}</span>
+        </div>
     </ng-container>
-  </ul>
-  
+  </div>
+
   <div *ngIf="selectedDevice">
-    <p>{{ selectedDevice.name }}</p>
-  </div>`,
-  styles: [`
+    <ul>
+      <li>{{ selectedDevice.signalStrength }}</li>
+      <li>{{ selectedDevice.battery }}</li>
+      <li>{{ selectedDevice.lastSeen }}</li>
+    </ul>
+  </div>
+  <h2>Systemstatus</h2>
+    <p>{{ devices_online(devices) }} % of the devices are online</p>
+    <p>{{ devices_offline(devices) }} % of the devices are offline</p>
+    <p>{{ warning_devices(devices) }} % of the devices have warnings</p>`,
+    styles: [`
     .list-item {
-      list-style: none;
       padding: 10px;
       border: 1px solid rgba(0,0,0,0.1);
       border-radius: 25px;
       margin-bottom: 10px;
       width: 25%;
-    }`]
+      display: flex;
+    }
+    
+    .spawn {
+      margin-left: auto;
+    }
+    
+    .location-box {
+      padding: 10px;
+      border: solid 1px rgba(0,0,0,0.2);
+    }
+    `]
 })
 
 export class AppComponent  { 
@@ -73,12 +122,74 @@ export class AppComponent  {
   }
 
   check_offline(device: any){
-    console.log(device);
-    let obj: Device = device;
-    if (obj.status == true){
+    if (device.status == false){
+      return true;
+    }
+  }
+
+  all_offline_devices(device: any){
+      if(device.status == false){
+        return device.name;
+      }
+    }
+
+
+    devices_online(devices: any) {
+        var count: number = 0;
+        for (let device of devices.devices) {
+            if (device.status == true) {
+                count = count + 1;
+            }
+        }
+        var online_percent: number = (count / devices.devices.length) * 100;
+        return Math.round(online_percent);
+  }
+
+    devices_offline(devices: any) {
+        var count: number = 0;
+        for (let device of devices.devices) {
+            if (device.status == false) {
+                count = count + 1;
+            }
+        }
+        var online_percent: number = (count / devices.devices.length) * 100;
+        return Math.round(online_percent);
+    }
+
+    warning_devices(devices: any) {
+        var count: number = 0;
+        for (let device of devices.devices) {
+            if ((device.battery < 20) && (device.battery > 0)) {
+                count = count + 1;
+            }
+        }
+        var online_percent: number = (count / devices.devices.length) * 100;
+        return Math.round(online_percent);
+    }
+
+
+  check_location(device: any){
+    //skapa ny lista för varje location och sätt location som header ovanför respektive lista
+    if(device["location"] == "rum22"){
+      return true;
+    }
+    
+  }
+
+  no_duplicates(device: any){
+    var mySelect: any = document.getElementById("device_location_list");
+    var mySelectLength: number = mySelect.length - 1;
+    console.log(mySelect);
+    if(mySelect.length != 0){
+      for(var i = 0; i < mySelect.length; i++){
+        if (mySelect.options[mySelectLength].value != device.location){
+          return true;
+        }
+      }
+    }
+    else{
       return true;
     }
   }
 
 }
-
