29c1f0c12a2aa460c86027ded74d1368faa13dff
Hofwimmer <felixhofwimmer@felixs-mbp.clients.internal.mah.se>
2017-05-22 17:23:12 +0200

    overview-all-devices

--- sigma/src/js/allDevices.js
+++ sigma/src/js/allDevices.js
@@ -1,3 +1,67 @@
+// at the top of your script create empty users var
+var users = [];
+
+// create a function that generates your endpoint url
+
+var urlWithUser = function(user){
+  return 'http://intelligentmonitoringapi.azurewebsites.net/api/positions/device/' + user;
+};
+
+
+// assign value to users
+var getInitialData = function(){
+    $.ajax({
+        type : 'GET',
+        url : 'http://intelligentmonitoringapi.azurewebsites.net/api/Devices',
+        dataType : 'json',
+        success : function(data) {
+            users = data.devices;
+            
+            $.each(users, function(index, value) {
+                 $.ajax({
+        type : 'GET',
+        url : urlWithUser(this.id),
+        dataType : 'json',
+        success : function(data) {
+            
+            
+            doSomething(data.y, data.x, value.contactLost);
+        },
+        error : function(code, message){
+            $('#error').html('Error Code: ' + code + ', Error Message: ' + message);            
+        }
+    });
+            }
+                   
+    )},
+        error : function(code, message){
+            $('#error').html('Error Code: ' + code + ', Error Message: ' + message);            
+        }
+    });
+};
+
+
+
+function doSomething(y, x, name) {
+var div2 = document.createElement("div");
+div2.style.top = y*100 + "%";
+div2.style.left = x*100 + "%";
+if (name == false){
+div2.style.background = "#00E538";      
+} else {
+div2.style.background = "red";       
+}
+div2.style.padding = "0.5em";       
+div2.style.borderRadius = "100%";       
+div2.style.position = "absolute";
+document.getElementById("test").appendChild(div2);   
+}
+
+// get first set of users
+setInterval(getInitialData, 1000);
+
+/*
+
 $.ajax({
     type: "GET",
     url: 'http://intelligentmonitoringapi.azurewebsites.net/api/Devices',
@@ -45,4 +109,4 @@ document.getElementById("test").appendChild(div2);
         
         }
     }
-    });
+    }); */
