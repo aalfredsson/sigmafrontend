8258bfd398bcf5aa59a829e7475dd5722b5e2b3d
Hofwimmer <felixhofwimmer@felixs-mbp.clients.internal.mah.se>
2017-05-22 17:37:34 +0200

    overview-devices-v2

--- sigma/src/js/allDevices.js
+++ sigma/src/js/allDevices.js
@@ -1,26 +1,21 @@
-// at the top of your script create empty users var
-var users = [];
+var devices = [];
 
-// create a function that generates your endpoint url
-
-var urlWithUser = function(user){
-  return 'http://intelligentmonitoringapi.azurewebsites.net/api/positions/device/' + user;
+var urlWithId = function(id){
+  return 'http://intelligentmonitoringapi.azurewebsites.net/api/positions/device/' + id;
 };
 
-
-// assign value to users
 var getInitialData = function(){
     $.ajax({
         type : 'GET',
         url : 'http://intelligentmonitoringapi.azurewebsites.net/api/Devices',
         dataType : 'json',
         success : function(data) {
-            users = data.devices;
+            devices = data.devices;
             
-            $.each(users, function(index, value) {
+            $.each(devices, function(index, value) {
                  $.ajax({
         type : 'GET',
-        url : urlWithUser(this.id),
+        url : urlWithId(this.id),
         dataType : 'json',
         success : function(data) {
             
@@ -40,8 +35,6 @@ var getInitialData = function(){
     });
 };
 
-
-
 function doSomething(y, x, name) {
 var div2 = document.createElement("div");
 div2.style.top = y*100 + "%";
@@ -57,56 +50,4 @@ div2.style.position = "absolute";
 document.getElementById("test").appendChild(div2);   
 }
 
-// get first set of users
 setInterval(getInitialData, 1000);
-
-/*
-
-$.ajax({
-    type: "GET",
-    url: 'http://intelligentmonitoringapi.azurewebsites.net/api/Devices',
-    dataType: "JSON",
-    error: function (response) {
-        alert('Error: There was a problem processing your request, please refresh the browser and try again');
-    },
-    success: function (response) {
-        
-
-        for (var i = 0; i < response.devices.length; i++) {
-            
-            var testUrl = "http://intelligentmonitoringapi.azurewebsites.net/api/positions/device/" + response.devices[i].id;
-            
-            var getDatas = response.devices[i].$id;
-            
-var div = document.createElement("div");
-div.style.position = "absolute";   
-div.innerHTML = getDatas;
-document.getElementById("test").appendChild(div);
-               
-$.ajax({
-    type: "GET",
-    url: testUrl,
-    dataType: "JSON",
-    error: function (response) {
-        alert('Error: There was a problem processing your request, please refresh the browser and try again');
-    },
-    success: function (response) {
-        
-var div2 = document.createElement("div");
-
-div2.style.top = response.y*100 + "%";
-div2.style.left = response.x*100 + "%";
-div2.style.background = "green";      
-div2.style.padding = "0.5em";       
-div2.style.borderRadius = "100%";       
-div2.style.position = "absolute";    
-document.getElementById("test").appendChild(div2);
-
-       
-   
-   }
-});     
-        
-        }
-    }
-    }); */
